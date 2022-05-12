11cfb55674a93f2d9dcfca1a4d87a58e772cf342
albertoncp <alberto.decela@bootcamp-upgrade.com>
2022-05-12 12:57:24 +0200

    firt-commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let myFavoriteHero = "hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = "h" + "y";
+
+console.log(z);
+
+let mult = 10 * 5;
+
+window.alert(mult);
+
+let divi = 10 / 2;
+
+window.alert(divi);
+
+let rest = 15 % 9;
+
+window.alert(rest);
+
+
+y = 10;
+z = 5;
+x = y + z;
+
+window.alert(x);
+
+x = 0;
+y = 10;
+z = 5;
+x = y * z;
+
+window.alert(x);
+
+
